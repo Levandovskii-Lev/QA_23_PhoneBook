8f1e0475afc21965e6fc7de559639e1f4eb83eac
Levandovskii-Lev <levlevisr@gmail.com>
2024-01-11 20:51:21 +0200

    login success

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,12 +1,36 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase {
 
+    @BeforeMethod
+    public void preConditions(){
+        if(app.getHelperUser().isLogged()){
+            app.getHelperUser().logout();
+        }
+    }
+
 @Test
-    public void test(){
-    
+    public void loginSuccess(){
+
+    app.getHelperUser().openLoginRegistrationForm();
+    app.getHelperUser().fillLoginRegistrationForm("mara@gmail.com", "Mmar123456$");
+    app.getHelperUser().submitLogin();
+
+    Assert.assertTrue(app.getHelperUser().isLogged());
 }
+
+    @Test
+    public void loginSuccessModel(){
+
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("mara@gmail.com", "Mmar123456$");
+        app.getHelperUser().submitLogin();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
+    }
 
 }
